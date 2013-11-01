e79cfbfda6276d817b32c3b4b14fc7facf3e64dc
Josh Bielick <josh@fragmentlabs.com>
2013-11-01 16:36:50 -0400

    redirect to login if not logged in

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -2,5 +2,5 @@ module.exports = function(req, res, next) {
 	if (req.session.user) {
 		return next();
 	}
-	return res.forbidden('You are not permitted to perform this action.');
+	return res.redirect('/login', 403);
 };
