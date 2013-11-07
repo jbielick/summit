5fd285a01ab5b2ce08e34a51dd95d14a60ab2e47
Josh Bielick <josh@fragmentlabs.com>
2013-11-06 20:49:23 -0500

    package include bcrypt

--- api/controllers/LogController.js
+++ api/controllers/LogController.js
@@ -42,14 +42,12 @@ module.exports = {
 		Log.findOne(req.param('id'), function(err, log) {
 			if (err) return res.send(500, err);
 			if (!log) return res.send(404);
-			console.log(log);
-			log = req.body;
-			console.log(log);
 			log.user_id = req.session.user.id;
-			log.save(function(err) {
+			_.each();
+			log.update(function(err) {
 				if (err) return res.send(500);
-				Log.publishUpdate(log.id, log);
-				res.json(log);
+				Log.publishUpdate(log.id, req.body);
+				res.json(log.toJSON());
 			});
 		})
 	}
