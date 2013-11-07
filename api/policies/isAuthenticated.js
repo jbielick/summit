c655ab855c0f2d6283d9d956e8acbaa0cd2cbbf4
Josh Bielick <josh@fragmentlabs.com>
2013-11-06 23:18:45 -0500

    resolve conflict

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -2,9 +2,6 @@ module.exports = function(req, res, next) {
 	if (req.session.user) {
 		return next();
 	}
-<<<<<<< HEAD
 	req.session.flash = 'Please login to continue.';
-=======
->>>>>>> dev
 	return res.redirect('/login');
 };
