2af19bc0f787ca5fcec322f5554b45a727c617b8
Josh Bielick <josh@fragmentlabs.com>
2013-11-13 10:38:17 -0500

    accept request only with body.data

--- api/models/Log.js
+++ api/models/Log.js
@@ -18,38 +18,40 @@ module.exports = {
 	},
 	createOrAppendChild: function(req, res, next) {
 		var crypto = require('crypto');
-		var hash = crypto.createHash('md5').update(req.body.data).digest('hex');
-		Log.findOne({hash: hash, site: req.body.site}, function(err, log) {
-			if (err) return console.log(err);
-			if (!log) {
-				// Create Log Record
-				req.body.hash = hash;
-				Site.findOne(req.body.site_id, function(err, site) {
-					if (err) return console.log(err);
-					if (site) {
-						req.body.Site = site;
-					}
-					Log.create(req.body).done(function(err, model) {
+		if (req.body.data) {
+			var hash = crypto.createHash('md5').update(req.body.data).digest('hex');
+			Log.findOne({hash: hash, site: req.body.site}, function(err, log) {
+				if (err) return console.log(err);
+				if (!log) {
+					// Create Log Record
+					req.body.hash = hash;
+					Site.findOne(req.body.site_id, function(err, site) {
 						if (err) return console.log(err);
-						res.status(201);
-						Log.publishCreate(model.toJSON());
-						next(null, model.toJSON());
-					});
-				})
-			} else {
-				if (!log.child_count) {
-					log.child_count = 1;
+						if (site) {
+							req.body.Site = site;
+						}
+						Log.create(req.body).done(function(err, model) {
+							if (err) return console.log(err);
+							res.status(201);
+							Log.publishCreate(model.toJSON());
+							next(null, model.toJSON());
+						});
+					})
 				} else {
-					log.child_count++;
+					if (!log.child_count) {
+						log.child_count = 1;
+					} else {
+						log.child_count++;
+					}
+					log.closed = false;
+					log.save(function(err) {
+						if (err) return console.log(err);
+						res.status(200);
+						Log.publishUpdate(log.id, log.toJSON());
+						next(null, log.toJSON());
+					})
 				}
-				log.closed = false;
-				log.save(function(err) {
-					if (err) return console.log(err);
-					res.status(200);
-					Log.publishUpdate(log.id, log.toJSON());
-					next(null, log.toJSON());
-				})
-			}
-		})
+			})
+		}
 	}
 };
