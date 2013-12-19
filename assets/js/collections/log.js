7bdca146f2f4a0ef62f837e2b07e3120910cda8f
Josh Bielick <josh@fragmentlabs.com>
2013-12-19 15:23:30 -0500

    try ordering of addition of models to feed collection

--- assets/js/collections/log.js
+++ assets/js/collections/log.js
@@ -20,23 +20,36 @@ function(Backbone, LogModel, LogView, LogTemplate, FeedUIView) {
 
 			this.temp = {$el: $('<div/>')};
 
-			this.on('add', function(model, collection) {
-				$(new LogView({template: _.template(LogTemplate, null, {variable: 'Log'}), model: model}).render().el).prependTo(_this.$el);
+			this.on('add', function(model, collection, options) {
+				var operation;
+				if (options.add) {
+					operation = 'prependTo';
+				} else if (options.merge) {
+					operation = 'appendTo';
+				}
+				$(new LogView({
+						template: _.template(LogTemplate, null, {variable: 'Log'}), 
+						model: model
+					}).render().el)[operation](_this.$el);
 			});
 			this.on('remove', function(model, collection) {
 				model.view.remove();
 			});
 			this.on('reset', function(collection, model) {
-				_this.sort({silent: true});
-				_this.temp.$el.empty();
-				_.each(collection.models, function(model) {
-					$(new LogView({template: _.template(LogTemplate, null, {variable: 'Log'}), model: model}).render().el).appendTo(_this.temp.$el)
-				});
-				if (_this.$el) {
-					_this.$el.html(this.temp.$el.html());
-				}
+				_this.refresh();
 				new FeedUIView({feed: _this});
 			});
+		},
+		refresh: function() {
+			var _this = this;
+			_this.sort({silent: true});
+			_this.temp.$el.empty();
+			_.each(_this.models, function(model) {
+				$(new LogView({template: _.template(LogTemplate, null, {variable: 'Log'}), model: model}).render().el).appendTo(_this.temp.$el)
+			});
+			if (_this.$el) {
+				_this.$el.html(this.temp.$el.html());
+			}
 		},
 		model: LogModel,
 		url: '/logs',
